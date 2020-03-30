7c544db0731d1dc6ff27f62a100c0a4260c6f991
Jonathon Reinhart <jreinhart@cc-sw.com>
2020-03-30 12:07:22 -0400

    wrapper: Log stderr to file and wait 1 second for early exit
    There are probably better ways to handle this, but this wrapper is already
    far too complex...

--- spicerecord/wrapper.py
+++ spicerecord/wrapper.py
@@ -3,15 +3,23 @@
 # of importing GLib into your program.
 import sys
 import subprocess
+import tempfile
 
 class SpiceRecordWrapper:
-    def __init__(self, dom, output=None):
+    def __init__(self, dom, output=None, uri=None):
         self.p = None
         self.stopped = False
+        self.stderr_file = tempfile.NamedTemporaryFile(
+                prefix='spice-record-stderr-', suffix='.txt')
 
         self.dom = dom
         self.output = output
         self.uri = uri
+
+    def _raise_failure(self):
+        self.stderr_file.seek(0)
+        errtxt = self.stderr_file.read().decode()
+        raise Exception("spice record failed ({}):\n{}".format(self.p.returncode, errtxt))
 
     def __enter__(self):
         pkg = '.'.join(__name__.split('.')[:-1])
@@ -29,14 +37,24 @@ class SpiceRecordWrapper:
                 args,
                 stdin=subprocess.PIPE,
                 stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
+                stderr=self.stderr_file,
                 )
+
+        # Briefly wait to make sure it didn't bomb early
+        try:
+            self.p.wait(timeout=1.0)
+        except subprocess.TimeoutExpired:
+            pass
+        else:
+            self._raise_failure()
+
         return self
 
     def __exit__(self, *exc_info):
         if not self.stopped:
             self.stop()
         self.wait()
+        self.stderr_file.close()
 
     def stop(self):
         if self.stopped:
@@ -50,5 +68,6 @@ class SpiceRecordWrapper:
             raise Exception("stop() not yet called")
         if not self.p:
             return
-        self.p.wait()
+        if self.p.wait() != 0:
+            self._raise_failure()
         self.p = None
